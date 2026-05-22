9a56d2dc2be6feb79c9ac53dde6f77fa69023f2a
Kayden_0811 <121278115+KaydenAu@users.noreply.github.com>
2026-05-22 15:35:20 +0800

    feat: capstone integration

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('app');
+  notes: any[] = [];
+
+  async ngOnInit() {
+    try {
+      const response = await fetch('/api/notes');
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      this.notes = await response.json();
+
+      console.log(this.notes);
+    } catch (error) {
+      console.error('Failed to fetch notes:', error);
+    }
+  }
 }
